2a532dc64851a5b9785a296aa2a42c66866d983e
Chris Strzelczyk <cstrzelc@hawk.nobletechnology.net>
2018-11-06 20:10:00 -0500

    reorganized script in OOP structure;easier to follow

--- mkproject.py
+++ mkproject.py
@@ -8,28 +8,15 @@
 #     3. TODO: download and install wordpress site instance (with wpcli)
 
 import argparse
+import configparser
 import os
 import platform
 import re
 import sys
 import subprocess
 
-""" Globals """
-vsite_conf_dir = '/etc/apache2/sites-available'  #default directory for configuration file
-vsite_conf_filename = ''                         #default name of sites-available file
-vsite_conf_file_fullpath = ''                    #default configuration filename
-vsite_conf_numeric_prefix = '00'                 #default numeric prefix
-vsite_default_docroot = '/var/www/html'          #default documentroot
-vsite_default_log_dir = '/var/log'               #default log directory
-                                                 #settings destined for the webserver configuration file
-vsite_settings = {'serveradmin':'webmaster@localhost', \
-                  'documentroot':vsite_default_docroot + '/default_name', \
-                  'errorlog':vsite_default_log_dir + '/default_error_log', \
-                  'accesslog':vsite_default_log_dir + 'default_access_log',
-                  }
-linux_dist_info = platform.dist()                #linux distro version
-
 """ binary locations """
+#TODO this will go into mkp_webserver()
 a2ensite = '/usr/sbin/a2ensite'
 
 """ Argument Handling """
@@ -37,6 +24,7 @@ parser = argparse.ArgumentParser(description='Make Noble Technology Project')
 parser.add_argument('-n', '--name', metavar='N', required=True, nargs='+', help='Specify the name of the project')
 parser.add_argument('-d', '--domain', metavar='N', required=True, nargs='+', help='Specify domain name for project')
 parser.add_argument('--hostfile', action='store_true', help='Specify domain name for project')
+parser.add_argument('-c', '--configfile', metavar='N', nargs='+', help='Specify alternate configuration file')
 args = parser.parse_args()
 
 name = args.name[0].strip()
@@ -49,72 +37,143 @@ except:
     print("The domain name {0} is not valid.".format(domain))
     sys.exit()
 hostfile = args.hostfile
+#validate configuration file;default is /root/.mkproject
+try:
+    configfile = args.configfile[0]
+    if not os.path.isfile(configfile):
+        print("The configuration file provided {0} does not exist".format(configfile))
+        sys.exit()
+except:
+    configfile = "/root/.mkproject"
+    if not os.path.isfile(configfile):
+        print("Configuration not specified and /root/.mkproject configuration does not exist. Exiting...")
+        sys.exit()
 
-#create the site name; default standard is 00-name.conf
-vsite_conf_filename = vsite_conf_numeric_prefix + '-' + name + '.conf'
-vsite_conf_file_fullpath = vsite_conf_dir + '/' + vsite_conf_numeric_prefix + '-' + name + '.conf'
-#reconfigure logs
-vsite_settings['errorlog'] = vsite_default_log_dir + '/' + name + '-error.log'
-vsite_settings['accesslog'] = vsite_default_log_dir + '/' + name + '-access.log'
+""" End Argument Handling """
 
+""" Define Object Classes """
+"""
+ - mkp_webserver() : detects webserver and performs various webserver configuration functions
+ - mkp_database()  : detects database and performs various database configurations
+ - mkp_os_actions(): performs various actions on the filesystem required to create project
+"""
 
-""" VERIFICATION CHECKS """
-#Check for exsitence of same-named webserver configuration file
-if os.path.isfile(vsite_conf_file_fullpath):
-    print("The file {0} already exists".format(vsite_conf_file_fullpath))
-    sys.exit()
+class mkp_webserver:
 
-""" Building Files """
-#build 00-name.conf
-def build_vsite_file(name, domain):
-    vsite_settings['documentroot'] = vsite_default_docroot + "/" + name
+    #Constructor
+    def __init__(self, config):
 
-    with open(vsite_conf_file_fullpath, 'w+') as vsite_avail_file:
-        vsite_avail_file.write("<VirtualHost " + domain + ":80>\n")
-        vsite_avail_file.write("ServerAdmin " + vsite_settings['serveradmin'] + "\n")
-        vsite_avail_file.write("DocumentRoot " + vsite_settings['documentroot'] + "\n")
-        vsite_avail_file.write("ErrorLog " + vsite_settings['errorlog'] + "\n")
-        vsite_avail_file.write("CustomLog " + vsite_settings['accesslog'] + " common\n")
-        vsite_avail_file.write("</VirtualHost>\n")
-
-
-""" Serverside Actions """
-#create documentroot directory
-def create_docroot(docroot_dir):
-    if not os.path.isdir(docroot_dir):
-        os.mkdir(docroot_dir)
-
-#enable configuration file
-def enable_vhost():
-    if linux_dist_info[0] == 'Ubuntu':
+        #pull webserver settings from configfile
         try:
-            result = subprocess.run([a2ensite, '-q', vsite_conf_filename])
+            config['webserver']['vsite_conf_dir']
+            self.vsite_conf_dir = config['webserver'].get('vsite_conf_dir')
         except:
-            print("Unable to enable the site for the Apache webserver")
+            #vsite_conf_dir is required
+            print("Unable to find vsite_conf_dir configuration.")
+            sys.exit()
+        try:
+            config['webserver']['vsite_conf_numeric_prefix']
+            self.vsite_conf_numeric_prefix = config['webserver']['vsite_conf_numeric_prefix']
+        except:
+            #default numeric prefix is '00'
+            self.vsite_conf_numeric_prefix = '00'
+            
+        self.vsite_conf_filename = self.vsite_conf_numeric_prefix + '-' + name + '.conf'
+        self.vsite_conf_file_fullpath = self.vsite_conf_dir + '/' + self.vsite_conf_numeric_prefix + '-' + name + '.conf'
+        #TODO wrap docroot and log_dir in try/except
+        self.vsite_default_docroot = '/var/www'          #default documentroot
+        self.vsite_default_log_dir = '/var/log'          #default log directory
+        self.linux_dist_info = linux_dist_info
+
+        #settings destined for the webserver configuration file                               
+        self.vsite_settings = {'serveradmin':'webmaster@localhost', \
+                  'documentroot':self.vsite_default_docroot + '/default_name', \
+                  'errorlog':self.vsite_default_log_dir + '/default_error_log', \
+                  'accesslog':self.vsite_default_log_dir + 'default_access_log',
+                  }
+        self.vsite_settings['errorlog'] = self.vsite_default_log_dir + '/' + name + '-error.log'
+        self.vsite_settings['accesslog'] = self.vsite_default_log_dir + '/' + name + '-access.log'
+
+        """ Verification checks"""
+        #Exit if the vhost configuration already exists
+        if os.path.isfile(self.vsite_conf_file_fullpath):
+            print("The file {0} already exists".format(self.vsite_conf_file_fullpath))
             sys.exit()
 
-#host file entry
-def enable_in_hostfile(domain):
-    domain_pattern = re.compile(domain)
+    #build vhost configuration file
+    def build_vsite_file(self, name, domain):
+        self.vsite_settings['documentroot'] = self.vsite_default_docroot + "/" + name
 
-    #check for existing entry
-    for i, line in enumerate(open('/etc/hosts')):
-        for match in re.finditer(domain_pattern, line):
-            print("Found existing entry for {0} in /etc/hosts.".format(domain))
-            return True
+        with open(self.vsite_conf_file_fullpath, 'w+') as vsite_avail_file:
+            vsite_avail_file.write("<VirtualHost " + domain + ":80>\n")
+            vsite_avail_file.write("ServerAdmin " + self.vsite_settings['serveradmin'] + "\n")
+            vsite_avail_file.write("DocumentRoot " + self.vsite_settings['documentroot'] + "\n")
+            vsite_avail_file.write("ErrorLog " + self.vsite_settings['errorlog'] + "\n")
+            vsite_avail_file.write("CustomLog " + self.vsite_settings['accesslog'] + " common\n")
+            vsite_avail_file.write("</VirtualHost>\n")
 
-    
-    with open('/etc/hosts', 'a') as hosts:
-        hosts.write("127.0.0.1 " + domain + "\n")
-    
+    #enable configuration file                                                                                                             
+    def enable_vhost(self):
+        if self.linux_dist_info[0] == 'Ubuntu':
+            try:
+                result = subprocess.run([a2ensite, '-q', self.vsite_conf_filename])
+            except:
+                print("Unable to enable the site for the Apache webserver")
+                sys.exit()
+
+    #return webserver settings
+    def get_webserver_settings(self):
+        return self.vsite_settings
+
+class mkp_database():
+
+    def __init__(self):
+        pass
+
+class mkp_os_actions():
+
+    def __init__(self):
+        pass
+
+    """ Serverside Actions """
+    #create documentroot directory                                                                                                    
+    def create_docroot(self, docroot_dir):
+        if not os.path.isdir(docroot_dir):
+            os.mkdir(docroot_dir)
+
+    #host file entry                                                                                                                       
+    def enable_in_hostfile(self, domain):
+        domain_pattern = re.compile(r'^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$')
+
+        #check for existing entry
+        for i, line in enumerate(open('/etc/hosts')):
+
+            for match in re.finditer(domain_pattern, line):
+                print("Found existing entry for {0} in /etc/hosts.".format(domain))
+                return True
+
+        with open('/etc/hosts', 'a') as hosts:
+            hosts.write("127.0.0.1 " + domain + "\n")
+
+#in Global namespace
+linux_dist_info = platform.dist()
 
 #execute main
 def main():
-    build_vsite_file(name, domain)
-    create_docroot(vsite_settings['documentroot'])
-    enable_vhost()
+    config = configparser.ConfigParser()             #create config parser object
+    config.read(configfile)         #read in project options
+    #dbpass=config['webserver']['vsite_conf_dir']            
+    
+    webserver = mkp_webserver(config)
+    os = mkp_os_actions()
+
+    webserver.build_vsite_file(name, domain)
+
+    vsite_settings = webserver.get_webserver_settings()
+    os.create_docroot(vsite_settings['documentroot'])
+    webserver.enable_vhost()
     if hostfile: 
-        enable_in_hostfile(domain)
+        os.enable_in_hostfile(domain)
 
 if __name__ == '__main__':
     main()
