15b5d72fb2610c16dffe818eff4a0b99b0e4a6f2
csaws <csaws@amazon.com>
2020-05-17 23:58:12 -0400

    adding vanilla httpd.conf file.  adding ServerName replacement code to provision script.

--- provision_nobletech_container.py
+++ provision_nobletech_container.py
@@ -12,6 +12,8 @@ logfile=working_directory + 'provision.log'
 home="/home/"
 update_customers_script='update_nobletech_customers.py'
 customers_database='/tmp/noble/website_dev_tools/customers.csv'
+uname=os.uname()
+httpdconf='/etc/httpd/conf/httpd.conf'
 
 now = datetime.now()
 today = now.strftime("%m/%d/%Y, %H:%M:%S")
@@ -24,57 +26,72 @@ def install_crontab(cronuser):
 
     cron.write()
 
+def modify_httpconf(httpdconf, hostname):
+    new_file_w_newline=""
+    ServerNameLine="#ServerName www.example.com:80"
+    reading_httpdconf = open(httpdconf, mode='r')
+    for line in reading_httpdconf:
+        stripped_line = line.strip()
+        new_line = stripped_line.replace(ServerNameLine, "ServerName " + hostname)
+        new_file_w_newline += new_line + "\n"
+    reading_httpdconf.close()
+
 # Provision customer environment based on customers.csv
 with open(customers_database, newline='') as csvfile:
     customerfile = csv.reader(csvfile, delimiter=',')
     for customer in customerfile:
-        customer_alias = customer[0]
-        customer_url = customer[1]
-        customer_email = customer[2]
-        customer_home = home + customer_alias + "/"
-        customer_www_root = home + customer_alias + "/public_html"
-        
-        os.system("echo 'Running " + customer_alias + " ' >>" + logfile)
-        
-        command = "mkdir -p  " + customer_www_root
+        if customer.startswith('#'):
+            continue
+        else:
+            customer_alias = customer[0]
+            customer_url = customer[1]
+            customer_email = customer[2]
+            customer_home = home + customer_alias + "/"
+            customer_www_root = home + customer_alias + "/public_html"
 
-        # Create customer directory
-        os.system(command)
+            os.system("echo 'Running " + customer_alias + " ' >>" + logfile)
 
-        # Download customer website source
-        try:
-            os.system("cd " + customer_www_root + ";git clone git@github-nobletech:cstrzelc/" + customer_alias + ".git . 2>>" + logfile)
-        except:
-            print("Unable to run git clone command.")
+            command = "mkdir -p  " + customer_www_root
 
-        # Apache conf.d configuration for customer
-        conffile = "/etc/httpd/conf.d/" + customer_alias + ".conf"
+            # Create customer directory
+            os.system(command)
 
-        seq1 = [ "< VirtualHost *: 80 >\n", "DocumentRoot " + customer_www_root + "\n", \
-                "serverAdmin " + customer_email + "\n", "ServerName " + customer_url + "\n",
-                "ErrorLog /var/log/httpd/" + customer_alias + "-error_log\n", \
-                "CustomLog /var/log/httpd/" + customer_alias + "-access_log\n", \
-                "< / VirtualHost >\n\n"
-                ]
+            # Download customer website source
+            try:
+                os.system("cd " + customer_www_root + ";git clone git@github-nobletech:cstrzelc/" + customer_alias + ".git . 2>>" + logfile)
+            except:
+                print("Unable to run git clone command.")
 
-        seq2 = [ "<Directory " + customer_www_root + " >\n", "AllowOverride FileInfo AuthConfig Limit Indexes\n", \
-                "Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec\n", \
-                "Require method GET POST OPTIONS\n", \
-                "</Directory>\n" ]
+            # Apache conf.d configuration for customer
+            conffile = "/etc/httpd/conf.d/" + customer_alias + ".conf"
 
-        # write configuration to Apache conf file
-        with open(conffile, "w") as conf:
-            conf.writelines( seq1 )
-            conf.writelines( seq2 )
+            seq1 = [ "<VirtualHost *:80>\n", "DocumentRoot " + customer_www_root + "\n", \
+                    "serverAdmin " + customer_email + "\n", "ServerName " + customer_url + "\n",
+                    "ErrorLog /var/log/httpd/" + customer_alias + "-error_log\n", \
+                    "CustomLog /var/log/httpd/" + customer_alias + "-access_log\n", \
+                    "</VirtualHost>\n\n"
+                    ]
 
-        # Download customer backup file
-        s3.Bucket('nobletech-sharedhosting-nightly-backup').download_file('provision-log', '/tmp/provision-log')
+            seq2 = [ "<Directory " + customer_www_root + " >\n", "AllowOverride FileInfo AuthConfig Limit Indexes\n", \
+                    "Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec\n", \
+                    "Require method GET POST OPTIONS\n", \
+                    "</Directory>\n" ]
+
+            # write configuration to Apache conf file
+            with open(conffile, "w") as conf:
+                conf.writelines( seq1 )
+                conf.writelines( seq2 )
+
+            # Download customer backup file
+            s3.Bucket('nobletech-sharedhosting-nightly-backup').download_file('provision-log', '/tmp/provision-log')
 
 #run_git_update="/usr/bin/python3 " + working_directory + update_customers_script
 #os.system(run_git_update)
 
 #Create crontab for doing regular github pulls
 install_crontab('root')
+#modify the main httpd.conf file with variables custom to this instance
+modify_httpconf(httpdconf, uname[1])
 
 
 
